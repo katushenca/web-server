e2302a69f13ef517e4b6a07aba6860db56fee524
katushenca <shutenko.katya@bk.ru>
2024-07-24 23:03:25 +0500

    first step to web server
    =)

---
+++ server.py
@@ -0,0 +1,23 @@
+import socket
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind(("127.0.0.1", 5252))
+server.listen(4)
+
+while True:
+    client, address = server.accept()
+    data = client.recv(1024).decode("utf-8")
+    print(data)
+    path = data.split(' ')[1]
+    if path == "/home":
+        response = \
+            "HTTP/1.1 200 OK\r\n" \
+            "Content-Type: text/html\r\n\r\n" \
+            "<html><body><h1>home</h1></body></html>"
+    else:
+        response = \
+            "HTTP/1.1 200 OK\r\n" \
+            "Content-Type: text/html\r\n\r\n" \
+            "<html><body><h1>hello</h1></body></html>"
+    client.sendall(response.encode("utf-8"))
+    client.close()
