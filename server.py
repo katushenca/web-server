238a5b176edcb596b4c4ea7423f179ce022a69e3
Elizaveta Pavlovskaya <lpavlovskayalp@mail.ru>
2024-07-29 15:06:22 +0500

    separate html files

--- server.py
+++ server.py
@@ -1,5 +1,9 @@
 import socket
 
+def read_html_file(file_path):
+    with open(file_path, 'r', encoding='utf-8') as file:
+        return file.read()
+
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind(("127.0.0.1", 5252))
 server.listen(4)
@@ -10,14 +14,11 @@ while True:
     print(data)
     path = data.split()[1]
     if path == "/home":
-        response = \
-            "HTTP/1.1 200 OK\r\n" \
-            "Content-Type: text/html\r\n\r\n" \
-            "<html><body><h1>home</h1></body></html>"
+        html_content = read_html_file("html files/home.html")
+    elif path == "/lizka":
+        html_content = read_html_file("html files/lizka.html")
     else:
-        response = \
-            "HTTP/1.1 200 OK\r\n" \
-            "Content-Type: text/html\r\n\r\n" \
-            "<html><body><h1>hello</h1></body></html>"
+        html_content = read_html_file("html files/hello.html")
+    response = f"HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n{html_content}"
     client.sendall(response.encode("utf-8"))
     client.close()
